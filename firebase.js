5d250b932bfcf2e18712efe089dfff51542a14ab
Jamin Chan <jaminchan321@gmail.com>
2024-07-31 21:26:03 -0700

    testing deploy

--- firebase.js
+++ firebase.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
 import { getAnalytics } from "firebase/analytics";
-import { myKeys } from "./firebaseKeys.js"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
